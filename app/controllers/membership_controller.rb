4adc553bd6e534b0b4466d1315e2ba6370e9210c
CeeFish <can.o.fisher@gmail.com>
2022-07-14 12:19:05 -0400

    Added font folder.

--- app/controllers/membership_controller.rb
+++ app/controllers/membership_controller.rb
@@ -1,31 +1,73 @@
 class MembershipsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :catch_not_found
+  before_action :set_membership, only: %i[ show edit update destroy ]
+  skip_before_action :authorized, only: [:new, :create]
 
+  # GET /memberships or /memberships.json
+  def index
+    @memberships = Membership.all
+    render json: @memberships
+  end
+
+  # GET /memberships/1 or /memberships/1.json
+  def show    
+  end
+
+  # GET /memberships/new
   def new
     @membership = Membership.new
   end
 
-  def create
-    @membership = Membership.new(membership_params)
-    if(@membership.save)
-        flash.notice = "The meetup record was created successfully."
-        redirect_to @meetup
-    else
-        flash.now.alert = @meetup.errors.full_messages.to_sentence
-        render :new  
-    end
-   end
+  # GET /memberships/1/edit
+  def edit
+  end
 
-  def delete
-    @membership = Membership.find_by(mem_params)
+  # POST /memberships or /memberships.json
+  def create
+   @membership = Membership.new(membership_params)
+    if @membership.save
+      session[:membership_id] = @membership.id
+      redirect_to @membership
+   else
+      flash.now.alert = @membership.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  # PATCH/PUT /memberships/1 or /memberships/1.json
+  def update
+      if @membership.update(membership_params)
+        flash.notice = "The membership record was updated successfully."
+        redirect_to @membership
+      else
+        flash.now.alert = @membership.errors.full_messages.to_sentence
+        render :edit
+      end
+  end
+
+  # DELETE /memberships/1 or /memberships/1.json
+  def destroy
     @membership.destroy
-    flash[:notice] = "Your membership has been DESTROYED"
-    redirect_to board_path(mem_params[:board_id])
+    respond_to do |format|
+      format.html { redirect_to memberships_url, notice: "Membership was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_membership
+      @membership = Membership.find(params[:id])
+    end
 
-  def membership_params
-    params.require(:membership).permit(:user_id, :board_id, :admin)
-  end
+    # Only allow a list of trusted parameters through.
+    def membership_params
+      params.require(:membership).permit(:user, :group, :meetup)
+    end
 
+    def catch_not_found(e)
+      Rails.logger.debug("We had a not found exception.")
+      flash.alert = e.to_s
+      redirect_to orders_path
+    end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   # belongs_to :user
   has_many :users
+  has_many :memberships
   
   validates :title, presence: true
   validates :location, presence: true
