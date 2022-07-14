628b654d3eedfee9eb4c2a0619d4b10c20eeb079
CeeFish <can.o.fisher@gmail.com>
2022-07-14 14:43:26 -0400

    Put rspec commands in for the user.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: "Jack Smith", email: "jsmith@sample.com") }
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+  it "is not valid without a name" do
+    subject.name=nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without an email" do
+    subject.email=nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid if the email address doesn't have a @" do
+    subject.email=nil
+    expect(subject).to_not be_valid
+  end
+  it "returns the correct name" do
+    expect(subject.name).to eq("Jack Smith")
+  end
 end
